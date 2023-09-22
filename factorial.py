4a5df33cc803cf7f9b47dca59c16ca12b2dd4ea4
bequbed <107822319+bequbed@users.noreply.github.com>
2023-09-22 16:32:44 +1200

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -1,15 +1,27 @@
 def factorial(n):
+    """
+    Calculate the factorial of a non-negative integer.
+
+    Args:
+        n (int): The number for which to calculate the factorial.
+
+    Returns:
+        int: The factorial of the input number.
+    """
+    if n < 0:
+        raise ValueError("Factorial is not defined for negative numbers.")
     if n == 0:
         return 1
     else:
-        return n * factorial(n - 1)
+        result = 1
+        for i in range(1, n + 1):
+            result *= i
+        return result
 
-# Input a number from the user
-num = int(input("Enter a number: "))
-
-# Calculate and print the factorial
-if num < 0:
-    print("Factorial is not defined for negative numbers.")
-else:
-    result = factorial(num)
-    print(f"The factorial of {num} is {result}")
+if __name__ == "__main__":
+    try:
+        num = int(input("Enter a non-negative integer: "))
+        result = factorial(num)
+        print(f"The factorial of {num} is {result}")
+    except ValueError as e:
+        print(e)
